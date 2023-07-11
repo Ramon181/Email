2e05802bf70440fb55fe68bbe4cd1bac0a375353
Ramon181 <ramonantonio29970476@gmail.com>
2023-07-11 17:30:05 -0500

    s

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
-const { USER, PASS } = process.env
+const { USER, PASS, PORT } = process.env
 
 const app = express();
-const port = 8000;
+const port = PORT || 8000;
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
